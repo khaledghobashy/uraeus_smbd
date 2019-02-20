fb7813cac0934b52624940ba385819c1a89d5df4
Khaled Ghobashy <khaled.ghobashy@live.com>
2019-02-20 10:51:34 +0200

    na

--- source/post_processors/temp_script.py
+++ source/post_processors/temp_script.py
@@ -4,7 +4,7 @@ scene = bpy.context.scene
 
 def clear_scene():
     for obj in bpy.context.scene.objects:
-        if obj.name != 'Lamp' or obj.name!='Camera':
+        if obj.name != 'Sun' or obj.name!='Camera':
             bpy.context.scene.objects.active = obj
             bpy.ops.object.delete()
 
@@ -35,8 +35,8 @@ def triad(v1):
     
 class cylinder_mesh(object):
     
-    def __init__(self,loc1,loc2,r,n=30):
-        
+    def __init__(self,name,loc1,loc2,r,n=30):       
+        self.name = name
         self.r = r
         self.n = n
         self.loc1 = loc1
@@ -44,12 +44,29 @@ class cylinder_mesh(object):
         self.axis = loc2 - loc1
         self.norm = self.axis/np.linalg.norm(self.axis)
             
-    def create_cylider(self):
+    def create_data(self):
         c1_verts = self._create_circle_verts(self.loc1)
         c2_verts = self._create_circle_verts(self.loc2)
         faces = self._create_cylinder_faces()
         self.data = {'verts':c1_verts+c2_verts,'faces':faces}
     
+    def show(self):
+        self.create_data()
+        verts = self.data['verts']
+        faces = self.data['faces']
+        mesh = bpy.data.meshes.new('%s_mesh'%self.name)
+        self.mesh = mesh
+        obj  = bpy.data.objects.new(self.name, mesh)
+        self.obj = obj
+        mesh.from_pydata(verts, [], faces)
+        mesh.update()
+        bpy.context.scene.objects.link(obj)
+        obj.rotation_mode = 'QUATERNION'
+        obj.select = True
+        bpy.context.scene.objects.active = obj
+        bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
+        
+        
     def _create_circle_verts(self,p):
         p = p[:,0]
         frame = triad(self.norm)
@@ -80,100 +97,29 @@ p3 = np.array([[-139],[215],[430]])*scale
 r = 0.5
 n = 30
 
+rocker_1 = cylinder_mesh('rocker_1',b1,s1,r,n)
+rocker_1.show()
+link_1 = cylinder_mesh('link_1',s1,p1,r,n)
+link_1.show()
 
-rocker_1 = cylinder_mesh(b1,s1,r,n)
-rocker_1.create_cylider()
-rocker_1_verts = rocker_1.data['verts']
-rocker_1_faces = rocker_1.data['faces']
-mesh_data = bpy.data.meshes.new("rocker_1_mesh")
-r1 = bpy.data.objects.new("rocker_1", mesh_data)
-mesh_data.from_pydata(rocker_1_verts, [], rocker_1_faces)
-mesh_data.update()
-scene.objects.link(r1)
-r1.select = True
-scene.objects.active = r1  # make the selection effective
+rocker_2 = cylinder_mesh('rocker_2',b2,s2,r,n)
+rocker_2.show()
+link_2 = cylinder_mesh('link_2',s2,p2,r,n)
+link_2.show()
 
-
-rocker_2 = cylinder_mesh(b2,s2,r,n)
-rocker_2.create_cylider()
-rocker_2_verts = rocker_2.data['verts']
-rocker_2_faces = rocker_2.data['faces']
-mesh_data = bpy.data.meshes.new("rocker_2_mesh")
-r2 = bpy.data.objects.new("rocker_2", mesh_data)
-mesh_data.from_pydata(rocker_2_verts, [], rocker_2_faces)
-mesh_data.update()
-scene.objects.link(r2)
-r2.select = True
-scene.objects.active = r2  # make the selection effective
-
-rocker_3 = cylinder_mesh(b3,s3,r,n)
-rocker_3.create_cylider()
-rocker_3_verts = rocker_3.data['verts']
-rocker_3_faces = rocker_3.data['faces']
-mesh_data = bpy.data.meshes.new("rocker_3_mesh")
-r3 = bpy.data.objects.new("rocker_3", mesh_data)
-mesh_data.from_pydata(rocker_3_verts, [], rocker_3_faces)
-mesh_data.update()
-scene.objects.link(r3)
-r3.select = True
-scene.objects.active = r3  # make the selection effective
+rocker_3 = cylinder_mesh('rocker_3',b3,s3,r,n)
+rocker_3.show()
+link_3 = cylinder_mesh('link_3',s3,p3,r,n)
+link_3.show()
 
 
-conn_1 = cylinder_mesh(s1,p1,r,n)
-conn_1.create_cylider()
-conn_1_verts = conn_1.data['verts']
-conn_1_faces = conn_1.data['faces']
-mesh_data = bpy.data.meshes.new("conn_1_mesh")
-c1 = bpy.data.objects.new("conn_1", mesh_data)
-mesh_data.from_pydata(conn_1_verts, [], conn_1_faces)
-mesh_data.update()
-scene.objects.link(c1)
-c1.select = True
-scene.objects.active = c1  # make the selection effective
-
-
-conn_2 = cylinder_mesh(s2,p2,r,n)
-conn_2.create_cylider()
-conn_2_verts = conn_2.data['verts']
-conn_2_faces = conn_2.data['faces']
-mesh_data = bpy.data.meshes.new("conn_2_mesh")
-mesh_data.from_pydata(conn_2_verts, [], conn_2_faces)
-mesh_data.update()
-c2 = bpy.data.objects.new("conn_2", mesh_data)
-c2.location = (88.5*scale,215*scale,215*scale)
-scene.objects.link(c2)
-c2.select = True
-scene.objects.active = c2  # make the selection effective
-
-conn_3 = cylinder_mesh(s3,p3,r,n)
-conn_3.create_cylider()
-conn_3_verts = conn_3.data['verts']
-conn_3_faces = conn_3.data['faces']
-mesh_data = bpy.data.meshes.new("conn_3_mesh")
-mesh_data.from_pydata(conn_3_verts, [], conn_3_faces)
-mesh_data.update()
-c3 = bpy.data.objects.new("conn_3", mesh_data)
-scene.objects.link(c3)
-c3.select = True
-scene.objects.active = c3 # make the selection effective
-
-bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
-
-r1.rotation_mode = 'QUATERNION'
-r2.rotation_mode = 'QUATERNION'
-r3.rotation_mode = 'QUATERNION'
-
-c1.rotation_mode = 'QUATERNION'
-c2.rotation_mode = 'QUATERNION'
-c3.rotation_mode = 'QUATERNION'
-
-
-with open('stewart_sim_data_v1.csv') as f:
+objects = [link_1.obj,link_2.obj,link_3.obj,rocker_1.obj,rocker_2.obj,rocker_3.obj]
+with open('stw_gou_sin_inputs.csv') as f:
     for i,line in enumerate(f):
         if i == 0: continue
         data = line.split(",")[15:]
         k = 0
-        for ob in [c1,c2,c3,r1,r2,r3]:
+        for ob in objects:
             ob.location = [float(n)*scale for n in data[k:k+3]]
             ob.rotation_quaternion = [float(n) for n in data[k+3:k+7]]
             ob.keyframe_insert('location', frame=i)
--- use_cases/simulations/stewart_sim.py
+++ use_cases/simulations/stewart_sim.py
@@ -14,15 +14,15 @@ from source.solvers.python_solver import solver
 f.SG.config.load_from_csv('stewart_mod_points_v3.csv')
 f.TR.config.load_from_csv('stewart_testrig_base_cfg_v1.csv')
 
-f.TR.config.AF_jcs_rev_1 = lambda t : np.deg2rad(360)*t
-f.TR.config.AF_jcs_rev_2 = lambda t : np.deg2rad(-360)*t
-f.TR.config.AF_jcs_rev_3 = lambda t : np.deg2rad(360)*t
+f.TR.config.AF_jcs_rev_1 = lambda t : np.sin(t)
+f.TR.config.AF_jcs_rev_2 = lambda t : np.sin(2*t)
+f.TR.config.AF_jcs_rev_3 = lambda t : np.sin(3*t)
 
 assm = f.numerical_assembly()
 assm.set_gen_coordinates(assm.q0)
 soln = solver(assm)
 
-time_array = np.linspace(0,2,250)
+time_array = np.linspace(0,2*np.pi,250)
 soln.solve_kds(time_array)
 
 
