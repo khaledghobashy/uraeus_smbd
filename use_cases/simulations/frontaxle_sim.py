517091efb6894fab2adff599ec0deeb4e0732728
Khaled Ghobashy <khaled.ghobashy@live.com>
2019-03-18 21:13:13 +0200

    added asurt_path variable

--- use_cases/simulations/frontaxle_sim.py
+++ use_cases/simulations/frontaxle_sim.py
@@ -21,10 +21,12 @@ f.SU.config = dwb_simple_points.configuration()
 f.ST.config = steer_simple_points.configuration()
 f.TR.config = frontaxle_testrig.configuration()
 
+#asurt_path = r'C:\Users\khaled.ghobashy\Desktop\Khaled Ghobashy\Mathematical Models\asurt_cdt_symbolic'
+asurt_path = r'E:\Main\asurt_cdt_symbolic'
 
-f.SU.config.load_from_csv(r'C:\Users\khaled.ghobashy\Desktop\Khaled Ghobashy\Mathematical Models\asurt_cdt_symbolic\use_cases\generated_templates\configurations\csv_files\dwb_st500_axletech.csv')
-f.ST.config.load_from_csv(r'C:\Users\khaled.ghobashy\Desktop\Khaled Ghobashy\Mathematical Models\asurt_cdt_symbolic\use_cases\generated_templates\configurations\csv_files\steer_st500_axletech.csv')
-f.TR.config.load_from_csv(r'C:\Users\khaled.ghobashy\Desktop\Khaled Ghobashy\Mathematical Models\asurt_cdt_symbolic\use_cases\generated_templates\configurations\csv_files\frontaxle_testrig_mod.csv')
+f.SU.config.load_from_csv(asurt_path + r'\use_cases\generated_templates\configurations\csv_files\dwb_st500_axletech.csv')
+f.ST.config.load_from_csv(asurt_path + r'\use_cases\generated_templates\configurations\csv_files\steer_st500_axletech.csv')
+f.TR.config.load_from_csv(asurt_path + r'\use_cases\generated_templates\configurations\csv_files\frontaxle_testrig_mod.csv')
 
 f.TR.config.AF_jcs_steer_gear = lambda t : 0*np.deg2rad(15)*np.sin(t)
 f.TR.config.AF_mcr_ver_act = lambda t : 170*np.sin(t)
@@ -32,23 +34,23 @@ f.TR.config.AF_mcl_ver_act = lambda t : 170*np.sin(t)
 f.TR.config.AF_jcr_rev = lambda t :  0*np.deg2rad(360)*t
 f.TR.config.AF_jcl_rev = lambda t : 0*-np.deg2rad(360)*t
 
-damping_data = lookup_table('damping')
-damping_data.read_csv(r'C:\Users\khaled.ghobashy\Desktop\Khaled Ghobashy\Data\ST500_Damping.csv')
-damping_func = damping_data.get_interpolator()
-def damping_func_mod(x):
-    return damping_func(x)*1e6
-f.SU.config.Fd_fal_strut = damping_func_mod
-f.SU.config.Fd_far_strut = damping_func_mod
-
-
-stiffness_data = lookup_table('stiffness')
-stiffness_data.read_csv(r'C:\Users\khaled.ghobashy\Desktop\Khaled Ghobashy\Data\ST500_GasSpring_IsoThermal.csv')
-stiffness_func = stiffness_data.get_interpolator()
-def stiffness_func_mod(x):
-    x = (x if x>=0 else 0)
-    return stiffness_func(x)*1e6
-f.SU.config.Fs_fal_strut = stiffness_func_mod
-f.SU.config.Fs_far_strut = stiffness_func_mod
+#damping_data = lookup_table('damping')
+#damping_data.read_csv(r'C:\Users\khaled.ghobashy\Desktop\Khaled Ghobashy\Data\ST500_Damping.csv')
+#damping_func = damping_data.get_interpolator()
+#def damping_func_mod(x):
+#    return damping_func(x)*1e6
+#f.SU.config.Fd_fal_strut = damping_func_mod
+#f.SU.config.Fd_far_strut = damping_func_mod
+#
+#
+#stiffness_data = lookup_table('stiffness')
+#stiffness_data.read_csv(r'C:\Users\khaled.ghobashy\Desktop\Khaled Ghobashy\Data\ST500_GasSpring_IsoThermal.csv')
+#stiffness_func = stiffness_data.get_interpolator()
+#def stiffness_func_mod(x):
+#    x = (x if x>=0 else 0)
+#    return stiffness_func(x)*1e6
+#f.SU.config.Fs_fal_strut = stiffness_func_mod
+#f.SU.config.Fs_far_strut = stiffness_func_mod
 
 
 #f.SU.config.far_strut_FL = 794
