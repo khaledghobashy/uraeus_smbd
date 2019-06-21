7f64cdc8e05e10f203a24db01c3f1690349a8edb
Khaled Ghobashy <khaled.ghobashy@live.com>
2019-06-21 14:26:48 +0200

    added triangular_prism

--- smbd/numenv/cpp_eigen/numerics/src/smbd/geometries.hpp
+++ smbd/numenv/cpp_eigen/numerics/src/smbd/geometries.hpp
@@ -15,3 +15,17 @@ public:
 
     cylinder_geometry(Eigen::Vector3d& p1, Eigen::Vector3d& p2, double& ro, double ri = 0);
 };
+
+
+
+class triangular_prism
+{
+public:
+    Eigen::Vector3d R ;
+    Eigen::Vector4d P ;
+    Eigen::Matrix3d J ;
+    Eigen::Matrix3d I ;
+    double m;
+
+    triangular_prism(Eigen::Vector3d& p1, Eigen::Vector3d& p2, Eigen::Vector3d& p3, double thickness);
+};
