7557d6cabd37dc77ced7f271101aea34e91d4985
Khaled Ghobashy <khaled.ghobashy@live.com>
2019-06-22 23:45:36 +0200

    adaptor to symbolic systems creation

---
+++ smbd/systems.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 20:58:47 2019
+
+@author: khaledghobashy
+"""
+
+from smbd._interfaces.systems import (standalone_topology, 
+                                      template_topology, 
+                                      assembly, configuration, load_pickled_data)
+
+__all__ = ['standalone_topology', 'template_topology', 'assembly',
+           'configuration', 'load_pickled_data']
+
