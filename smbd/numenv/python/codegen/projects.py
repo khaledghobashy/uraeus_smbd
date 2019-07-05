227ba1818da6c8ff3b9a50449917b8bea597a53d
Khaled Ghobashy <khaled.ghobashy@live.com>
2019-07-05 22:19:27 +0200

    added templatebased_project

--- smbd/numenv/python/codegen/projects.py
+++ smbd/numenv/python/codegen/projects.py
@@ -69,26 +69,28 @@ class standalone_project(object):
                 from src import topology, configuration
                 
                 
-                num_model = multibody_system(topology)
-                num_model.topology.config = configuration.configuration()
+                num_model  = multibody_system(topology)
+                num_config = configuration.configuration()
                 
-                inputs_df = pd.read_csv('path/to/config.csv', index_col=0)
+                num_model.topology.config = num_config
+                
+                inputs_df = pd.read_csv('../../config_inputs/config.csv', index_col=0)
                 # input the configuration data here ...
                 inputs_df.loc['P_ground'] = [1, 0, 0, 0]
                 
                 
                 # Saving the configuration as a .csv file.
-                inputs_df.to_csv('path/to/new.csv')
+                inputs_df.to_csv('../../config_inputs/new.csv')
                 
-                num_model.topology.config.load_from_dataframe(inputs_df)
+                num_config.load_from_dataframe(inputs_df)
                 
                 # Setting actuation data
-                #num_model.topology.config.UF_mcs_act_1 = lambda t :  np.deg2rad(360)*t
+                #num_config.UF_mcs_act_1 = lambda t :  np.deg2rad(360)*t
            
-                sim = simulation('sim1', num_model, 'kds')
+                sim = simulation('sim', num_model, 'kds')
                 sim.set_time_array(1, 100)
                 sim.solve()
-                sim.save_results('../../config_inputs', 'sim1')
+                sim.save_results('../../results', 'sim')
             
         '''        
         text = text.expandtabs()
@@ -114,4 +116,54 @@ class standalone_project(object):
         with open(file_name, 'w') as file:
             file.write('#')
         
+
+
+class templatebased_project(object):
     
+    def __init__(self, database_dir, topology, config):
+        
+        self._parent_dir = os.path.abspath(database_dir)
+        self._topology = topology.topology
+        self._config = config.config
+
+        self._template_name = topology.name
+        self._code_dir = os.path.join(self._parent_dir, 'numenv', 'python')
+        self._templates_dir  = os.path.join(self._code_dir, 'templates')
+        self._assemblies_dir = os.path.join(self._code_dir, 'assemblies')
+    
+    def create(self):
+        self.create_dirs()
+        self.write_topology_code()
+        self.write_configuration_code()
+    
+    
+    def create_dirs(self, clean=False):
+        if os.path.exists(self._code_dir):
+            if clean:
+                shutil.rmtree(self.code_dir)
+                self._create_template_dir()
+                self._write_init_file()
+        self._create_template_dir()
+        self._write_init_file()
+            
+        
+    def write_topology_code(self):
+        src_path = self._source_dir
+        codegen = generators.template_codegen(self._topology)
+        codegen.write_code_file(src_path)
+    
+    
+    def write_configuration_code(self):
+        src_path = self._source_dir
+        codegen = generators.configuration_codegen(self._config)
+        codegen.write_code_file(src_path)
+        
+    def _create_template_dir(self):
+        self._source_dir =  os.path.join(self._templates_dir, self._template_name)
+        if not os.path.exists(self._source_dir):
+            os.makedirs(self._source_dir)
+    
+    def _write_init_file(self):
+        pass
+
+   
