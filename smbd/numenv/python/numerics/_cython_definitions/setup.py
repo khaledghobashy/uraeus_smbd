27a346bd0d1d5572c6059b1ec96fde63a13008a6
Ali Mohamed <ali.mohamed@i-mut.local>
2019-09-10 16:39:42 +0200

    compiled cython scripts - need to add .gitignore in future and do a clean-up

--- smbd/numenv/python/numerics/_cython_definitions/setup.py
+++ smbd/numenv/python/numerics/_cython_definitions/setup.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 import numpy as np
 
 setup(name='matrix_funcs',
-      ext_modules=cythonize("matrix_funcs.pyx"),
-      include_dirs = [np.get_include()],)
+      ext_modules=cythonize("matrix_funcs.pyx", compiler_directives={'language_level' : "3"}),
+      include_dirs = [np.get_include()])
