7e7fcf3c6a87b95a0d912fd43e5d52c43b582b4b
Ali Mohamed <ali.mohamed@i-mut.local>
2019-09-24 14:58:03 +0200

    minor changes

--- smbd/numenv/python/numerics/core/math_funcs/__init__.py
+++ smbd/numenv/python/numerics/core/math_funcs/__init__.py
@@ -5,7 +5,7 @@ try:
 except ModuleNotFoundError:
     print('Failed importing compiled Cython matrices!')
     print('Falling back to pure python mode.')
-    from numba_funcs import A, B, G, E, skew_matrix as skew
+    from .numba_funcs import A, B, G, E, skew_matrix as skew
     from .misc import triad
 
 
