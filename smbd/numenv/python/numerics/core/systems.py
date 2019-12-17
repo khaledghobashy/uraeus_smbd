003c0169157be2afc6f243ed347c77995bf4e80d
Khaled Ghobashy <khaled.ghobashy@live.com>
2019-12-17 16:39:05 +0200

    added configuration loader from JSON file for more modularity.

--- smbd/numenv/python/numerics/core/systems.py
+++ smbd/numenv/python/numerics/core/systems.py
@@ -6,13 +6,17 @@ Created on Wed Mar 27 08:49:16 2019
 """
 # Standard library imports
 import os
+import json
 import pickle
+import itertools
 
 # 3rd party library imports
 import numpy as np
 
 # Local applicataion imports
 from .solvers import kds_solver, dds_solver
+from smbd.utilities.numerics.spatial_alg import centered, oriented, mirrored
+from smbd.utilities.numerics.geometries import cylinder_geometry
 
 ###############################################################################
 
@@ -64,4 +68,110 @@ class simulation(object):
     def eval_reactions(self):
         self.soln.eval_reactions()
     
+
+###############################################################################
+###############################################################################
+
+class JSON_configuration(object):
+
+    _construction_map = {'array': np.array,
+                         'getattribute': getattr,
+                         'Oriented': oriented,
+                         'Mirrored': mirrored,
+                         'Centered': centered,
+                         'Cylinder_Geometry': cylinder_geometry,}
+
+    def __init__(self, json_file):
+        self.file = json_file
+        self.construct_toplevel_dicts()
+        self.construct_inputs()
+
+    def construct_toplevel_dicts(self):
+        json_text = self.file
+        data_dict = json.loads(json_text)
+
+        self.user_inputs = data_dict['user_inputs']
+        self.evaluations = data_dict['evaluations']
+        self.outputs     = data_dict['outputs']
+    
+    def construct_inputs(self):
+        user_inputs_dict = self.user_inputs
+        for key, data in user_inputs_dict.items():
+            if isinstance(data, dict):
+                constructor = self._construction_map[data['constructor']]
+                args = data['args']
+                value = constructor(args)[:, None]
+                setattr(self, key, value)
+            elif isinstance(data, (int, float, str, bool)):
+                setattr(self, key, data)
+    
+    def perform_evaluations(self):
+        
+        evaluations_dict = self.evaluations
+        outputs_dict = self.outputs
+        
+        for key, data in evaluations_dict.items():
+            if isinstance(data, dict):
+                constructor = self._construction_map[data['constructor']]
+                args = [getattr(self, arg) for arg in data['args']]
+                value = constructor(*args)
+                setattr(self, key, value)
+            elif isinstance(data, (int, float, str, bool)):
+                setattr(self, key, data)
+        
+        for key, data in outputs_dict.items():
+            if isinstance(data, dict):
+                constructor = self._construction_map[data['constructor']]
+                if constructor is getattr:
+                    args = [getattr(self, data['args'][0]), data['args'][1]]
+                else:
+                    args = [getattr(self, arg) for arg in data['args']]
+                value = constructor(*args)
+                setattr(self, key, value)
+            elif isinstance(data, (int, float, str, bool)):
+                if isinstance(data, str):
+                    value = getattr(self, data)
+                else:
+                    value = data
+                setattr(self, key, value)
+
+
+###############################################################################
+###############################################################################
+
+class configuration(object):
+    
+    def __init__(self, name):
+        self.name = name
+
+        self.R_ground = np.array([[0], [0], [0]], dtype=np.float64)
+        self.P_ground = np.array([[1], [0], [0], [0]], dtype=np.float64)
+
+        self.Rd_ground = np.array([[0], [0], [0]], dtype=np.float64)
+        self.Pd_ground = np.array([[0], [0], [0], [0]], dtype=np.float64)
+    
+    def construct_from_json(self, json_file, assemble=False):
+
+        json_config = JSON_configuration(json_file)
+        self.json_config = json_config
+
+        if not assemble:
+            _attributes = json_config.user_inputs.keys()
+            for key in _attributes:
+                value = getattr(json_config, key)
+                setattr(self, key, value)
+        else:
+            self.assemble()
+        
+        
+    
+    def assemble(self):
+        self.json_config.perform_evaluations()
+
+        _attributes = itertools.chain(self.json_config.evaluations.keys(),
+                                      self.json_config.outputs.keys())
+        
+        for key in _attributes:
+            value = getattr(self.json_config, key)
+            setattr(self, key, value)
 
