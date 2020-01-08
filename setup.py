ebb510d326570839f51d344ce91deb95e9401cf6
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-01-08 16:30:47 +0200

    modified setup.py to handle namespace package

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ short_description = 'Symbolic creation and analysis of constrained multi-body sy
 
 setuptools.setup(
     name = "uraeus.smbd",
+    namespace_packages=['uraeus'],
     version = "0.0.dev1",
     author = "Khaled Ghobashy",
     author_email = "khaled.ghobashy@live.com",
@@ -14,7 +15,7 @@ setuptools.setup(
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/khaledghobashy/uraeus",
-    packages = setuptools.find_packages(exclude=("tests",)),
+    packages = ['uraeus'] + setuptools.find_packages(exclude=("tests",)),
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD 3-Clause License",
