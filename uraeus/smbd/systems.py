94805fc73f8c062827e090d2ecf857ab4b62dbff
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-01-19 11:53:31 +0200

    removed white spaces

--- uraeus/smbd/systems.py
+++ uraeus/smbd/systems.py
@@ -7,14 +7,14 @@ Created on Sat Jun 22 20:58:47 2019
 """
 
 from .utilities.interfaces.systems import (standalone_topology, 
-                                               template_topology, 
-                                               assembly, configuration, 
-                                               load_pickled_data)
+                                           template_topology, 
+                                           assembly, configuration, 
+                                           load_pickled_data)
         
 
 from .utilities.interfaces.projects import (smbd_database,
-                                                 standalone_project,
-                                                 templatebased_project)
+                                            standalone_project,
+                                            templatebased_project)
 
 __all__ = ['standalone_topology', 'template_topology', 'assembly',
            'configuration', 'load_pickled_data', 'standalone_project',
