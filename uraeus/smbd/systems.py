a851c11f61d5aea2c47d2d4fe3f7490ac27493ab
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-03-27 11:21:29 +0200

    updated systems.py to not import removed modules

--- uraeus/smbd/systems.py
+++ uraeus/smbd/systems.py
@@ -11,12 +11,6 @@ from .utilities.interfaces.systems import (standalone_topology,
                                            assembly, configuration, 
                                            load_pickled_data)
         
-
-from .utilities.interfaces.projects import (smbd_database,
-                                            standalone_project,
-                                            templatebased_project)
-
 __all__ = ['standalone_topology', 'template_topology', 'assembly',
-           'configuration', 'load_pickled_data', 'standalone_project',
-           'templatebased_project', 'smbd_database']
+           'configuration', 'load_pickled_data']
 
