668af43abcd78ec8183fcd2b71ef850670e8f17c
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-08-06 09:42:54 +0200

    added master_doc = 'index'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Khaled Ghobashy'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1.dev3'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
