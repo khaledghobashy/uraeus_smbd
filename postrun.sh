fda9f539b7cc539e09addac00f7eb61c3ee466a9
Khaled Ghobashy <khaled.ghobashy@live.com>
2023-11-15 19:22:26 +0000

    devcontainer setup

---
+++ postrun.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo "Initializing postCreate setup!"
+pip install --upgrade pip
+pip install black
+pip install -e .
--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ import setuptools
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
-short_description = 'A python package for symbolic creation and analysis of constrained multi-body systems.'
+short_description = "A python package for symbolic creation and analysis of constrained multi-body systems."
 
 setuptools.setup(
-    name = "uraeus.smbd",
-    namespace_packages=['uraeus'],
-    version = "0.0.1.dev3",
-    author = "Khaled Ghobashy",
-    author_email = "khaled.ghobashy@live.com",
-    description = short_description,
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/khaledghobashy/uraeus_smbd",
-    packages = ['uraeus'] + setuptools.find_packages(exclude=("tests",)),
-    classifiers = [
+    name="uraeus.smbd",
+    namespace_packages=["uraeus"],
+    version="0.0.1.dev3",
+    author="Khaled Ghobashy",
+    author_email="khaled.ghobashy@live.com",
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/khaledghobashy/uraeus_smbd",
+    packages=["uraeus"] + setuptools.find_packages(exclude=("tests",)),
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
@@ -25,13 +25,8 @@ setuptools.setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering :: Mathematics"
+        "Topic :: Scientific/Engineering :: Mathematics",
     ],
-    python_requires = '>=3.6',
-    install_requires=[
-          'sympy==1.4',
-          'networkx',
-          'cloudpickle',
-          'matplotlib'
-      ],
+    python_requires=">=3.6",
+    install_requires=["sympy", "networkx", "cloudpickle", "matplotlib"],
 )
